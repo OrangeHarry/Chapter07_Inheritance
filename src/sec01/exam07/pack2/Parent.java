5df7c0d1a84ef8805ad9403014b96a2775f8afd3
hhm12 <hhm12@LAPTOP-08SROAN1>
2021-08-05 09:36:12 +0900

    study

---
+++ src/sec01/exam07/pack2/Parent.java
@@ -0,0 +1,14 @@
+package sec01.exam07.pack2;
+
+public class Parent {
+	public String nation;
+	
+	public Parent() {
+		this("¥Î«—πŒ±π");
+		System.out.println("2222222222");
+	}
+	public Parent(String nation) {
+		this.nation = nation;
+		System.out.println("1111111111111");
+	}
+}
---
+++ src/sec01/exam07/pack2/Test.java
@@ -0,0 +1,7 @@
+package sec01.exam07.pack2;
+
+public class Test {
+	public static void main(String[] args) {
+		Parent parent = new Parent();
+	}
+}
